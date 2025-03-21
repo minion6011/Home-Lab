3def76f5e46298ee46da36a056fecd6dcbc750a7
Minion6011 <mvittorio29@gmail.com>
2025-03-21 18:27:11 +0100

    Create music.py

---
+++ code/HomeLab/website/music_funct/music.py
@@ -0,0 +1,12 @@
+import pytubefix
+from pytubefix.cli import on_progress
+
+
+def download_youtube(url):
+	if url.startswith("https://youtu.be/"):
+		url = url.replace("https://youtu.be/", "https://youtube.com/watch?v=")
+	else:
+		url = url
+	yt = pytubefix.YouTube(url, on_progress_callback = on_progress, client="WEB")
+	file_name = f"{yt.title}.mp3"
+	yt.streams.first().download(filename=file_name, output_path="/home/coso/Desktop/HomeLab/website/music_funct")
