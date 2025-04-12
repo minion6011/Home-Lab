b7d353298886c54e0eee9b2bae08f8f016db4540
Minion6011 <mvittorio29@gmail.com>
2025-04-12 11:23:52 +0200

    Bug fix

--- code/HomeLab/website/music_funct/music.py
+++ code/HomeLab/website/music_funct/music.py
@@ -1,6 +1,6 @@
 import pytubefix
 from pytubefix.cli import on_progress
-
+import os 
 
 def download_youtube(url):
 	if url.startswith("https://youtu.be/"):
@@ -9,4 +9,5 @@ def download_youtube(url):
 		url = url
 	yt = pytubefix.YouTube(url, on_progress_callback = on_progress)
 	file_name = f"{yt.title}.mp3"
-	yt.streams.first().download(filename=file_name)
+	path = os.path.abspath(os.getcwd()) + "/website/music_funct/"
+	yt.streams.first().download(filename=file_name, output_path=path)
