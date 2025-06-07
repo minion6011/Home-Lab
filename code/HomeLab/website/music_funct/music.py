5dcd5c6482688a0561702bd52951749394ed90af
Minion6011 <mvittorio29@gmail.com>
2025-06-07 20:42:09 +0200

    Update music.py

--- code/HomeLab/website/music_funct/music.py
+++ code/HomeLab/website/music_funct/music.py
@@ -5,8 +5,6 @@ import os
 def download_youtube(url):
 	if url.startswith("https://youtu.be/"):
 		url = url.replace("https://youtu.be/", "https://youtube.com/watch?v=")
-	else:
-		url = url
 	yt = pytubefix.YouTube(url, on_progress_callback = on_progress)
 	file_name = f"{yt.title}.mp3"
 	path = os.path.abspath(os.getcwd()) + "/website/music_funct/"
